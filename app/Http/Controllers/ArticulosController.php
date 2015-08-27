6c5e98452cfa34c640161aeb4ef6e7b289af349a
Miguel Mendez <keloxers@gmail.com>
2015-08-27 20:21:48 -0300

    repair view all

--- app/Http/Controllers/ArticulosController.php
+++ app/Http/Controllers/ArticulosController.php
@@ -127,21 +127,14 @@ class ArticulosController extends Controller
                       $articulo->url_foto = $filenew;
     	    }
 
-
-
-
         }
-
-
-
-
-
-
-
 
         $articulo->save();
 
-        $articulos = Articulo::all();
+        $articulos = Articulo::where('users_id', Auth::user()->id)
+                         ->orderBy('created_at', 'desc')
+                         ->paginate(20);
+
         return view('articulos.index', ['articulos' => $articulos]);
 
 
